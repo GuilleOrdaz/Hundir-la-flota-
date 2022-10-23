ade1611c972d94c9eadfe76ef9a61621eee2fe46
María de los Ángeles Naranjo Muñoz <113980137+Maringels@users.noreply.github.com>
2022-10-23 18:39:42 +0200

    Add files via upload

--- jugador2.py
+++ jugador2.py
@@ -1,27 +1,31 @@
 import random
 import jugador1
-def disparo_player2(tablero_auto_disp,tablero_vacio2,tablero_jugador_disp, tablero_vacio):
-        while 'O' in tablero_auto_disp and 'O' in  tablero_jugador_disp:
-            print(tablero_auto_disp)
-            print('Tablero de la maquina\n',tablero_vacio2)
-            x = random.randint(0,9)
-            if x < 0 or x > 9:
-                print('Num. no valido, tira otra vez')
-                continue
-            y = random.randint(0,9)
-            if y < 0 or y > 9:
-                print('Num. no valido, tira otra vez')
-                continue
+import numpy as np
+import time
+tablero = np.full((10, 10), ' ')
+tablero_auto_disp = tablero.copy()
+tablero_jugador_disp = tablero.copy()
 
-            if tablero_auto_disp[x, y] == 'O':
-                tablero_auto_disp[x, y] = 'X'
-                tablero_vacio2[x, y] = 'X'
-                print("Tablero de la maquina\n", 'Has dado en el blanco!\n', tablero_vacio2)
-                continue
-            elif tablero_auto_disp[x, y] == ' ':
-                tablero_auto_disp[x, y] = 'A'
-                tablero_vacio2[x, y] = 'A'
-                print("Tablero de la maquina\n", 'Has hecho Agua!\n', tablero_vacio2)
-                jugador1.disparo_player1(tablero_jugador_disp, tablero_vacio,tablero_auto_disp,tablero_vacio2)
-            else:
-                print('¡HAS GANADO!')
+tablero_vacio = np.full((10,10,), " ") 
+tablero_vacio2 = np.full((10,10,), " ")
+
+def disparo_player2(tablero_auto_disp,tablero_vacio2,tablero_jugador_disp, tablero_vacio):
+   
+    while 'O' in tablero_auto_disp and 'O' in  tablero_jugador_disp:
+        
+        x = random.randint(0,9)
+        y = random.randint(0,9)
+
+        if tablero_auto_disp[x, y] == 'O':
+            tablero_auto_disp[x, y] = 'X'
+            tablero_vacio2[x, y] = 'X'
+            print("Tablero de la maquina\n", 'El adversario ha dado en el BLANCO!\n', tablero_auto_disp)
+            continue
+        elif tablero_auto_disp[x, y] == ' ':
+            tablero_auto_disp[x, y] = '-'
+            tablero_vacio2[x, y] = '-'
+            print("Tablero de la maquina\n", 'La maquina ha dado en el AGUA!\n', tablero_auto_disp)
+            time.sleep(2)
+            jugador1.disparo_player1(tablero_jugador_disp, tablero_vacio,tablero_auto_disp,tablero_vacio2)
+        else:
+            continue
